5b65d50d20db5118c6892b8e3db3ef0b4627704c
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-02-13 11:42:27 -0500

    adding initial query and mutation for item

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if user logged in
+    
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info);
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,15 @@
-const Query = {};
+const { forwardTo } = require("prisma-binding");
+
+const Query = {
+  items: forwardTo('db'),
+  // when simply passing a request on to the database (not doing any checks prior to),
+  // can use forwardTo, and do not need to write the explicit code below
+  //
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+
+  //   return items;
+  // }
+};
 
 module.exports = Query;
