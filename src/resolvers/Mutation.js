3cf0e3faa4d2d1925db1f19986e732d50eecbcef
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-02-15 13:40:14 -0500

    add update item query and mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -9,6 +9,23 @@ const Mutations = {
     }, info);
 
     return item;
+  },
+  updateItem(parent, args, ctx, info) {
+    // TODO: check if user logged in
+    
+    // first make a copy of the updates
+    const updates = {...args};
+    // then remove id (don't want to update that)
+    delete updates.id;
+    
+    return ctx.db.mutation.updateItem({
+      data: {
+        ...updates
+      },
+      where: {
+        id: args.id
+      }
+    }, info);
   }
 };
 
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,6 +2,7 @@ const { forwardTo } = require("prisma-binding");
 
 const Query = {
   items: forwardTo('db'),
+  item: forwardTo('db'),
   // when simply passing a request on to the database (not doing any checks prior to),
   // can use forwardTo, and do not need to write the explicit code below
   //
