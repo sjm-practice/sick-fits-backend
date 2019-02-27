502f585e6e48285ed7d3a0a382c425c29f9b6661
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-02-27 15:20:04 -0500

    add signin

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -44,7 +44,7 @@ const Mutations = {
     // hash the password
     const password = await bcrypt.hash(args.password, 10);
     // create the user in the db
-    const user = ctx.db.mutation.createUser(
+    const user = await ctx.db.mutation.createUser(
       {
         data: {
           ...args,
@@ -63,6 +63,31 @@ const Mutations = {
     });
 
     // return user to browser
+    return user;
+  },
+  async signin(parent, {email, password}, ctx, info) {
+    // 1. check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email }});
+    if (!user) {
+      throw new Error(`No such user for email ${email}.`);
+    }
+
+    // 2. check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error("Invalid password.");
+    }
+
+    // 3. generate the JWT token
+    const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
+    
+    // 4. set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+    });
+    
+    // 5. return the user
     return user;
   }
 };
