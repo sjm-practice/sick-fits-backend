a4fcba4c1cecd90302d25bd92746bb1ca4422246
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-03-04 11:05:20 -0500

    add relation between item and user

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -6,12 +6,21 @@ const { transport, emailTemplate } = require("../mail");
 
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO: check if user logged in
+    // check if user logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in to create an item to sell!');
+    }
     
     const item = await ctx.db.mutation.createItem({
       data: {
+        // Prisma: create a relationship between item and user
+        user: {
+          connect: {
+            id: ctx.request.userId,
+          },
+        },
         ...args
-      }
+      },
     }, info);
 
     return item;
